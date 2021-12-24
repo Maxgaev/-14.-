ad9568509661c2b5550666952157447741903903
Максим Алехин <Max-crf@mail.ru>
2021-12-24 15:38:42 +0700

    Добавьте файлы проекта.

---
+++ Задание 14. Абстрактные классы/Animal.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_14.Абстрактные_классы
+{
+    abstract class Animal
+    {
+        
+        public abstract string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public abstract void Say();
+        public void ShowInfo()
+        {
+            Console.Write($"\nПривет, меня зовут {Name}, ");
+            Say();
+        }
+    }
+    class Cat : Animal
+    {
+        string name;
+        public Cat(string name)
+            : base(name)
+        {
+
+        }
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public override void Say()
+        {
+            Console.Write("Мяу!");
+        }
+    }
+    class Dog : Animal
+    {
+        string name;
+        public Dog(string name)
+            : base(name)
+        {
+
+        }
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public override void Say()
+        {
+            Console.Write("Гав!");
+        }
+    }
+}
+
---
+++ Задание 14. Абстрактные классы/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_14.Абстрактные_классы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя кошки:");
+            string catName = Console.ReadLine();
+            Console.WriteLine("\nВведите имя собаки:");
+            string dogName = Console.ReadLine();
+            Cat cat = new Cat(catName);
+            cat.ShowInfo();
+            Dog dog = new Dog(dogName);
+            dog.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
